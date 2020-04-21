5e4114dfedf7cd0cdfef2bcc8f19aa64e701b2e1
Maruti Jamdade <maruti.3003@gail.com>
2020-04-21 20:23:37 +0530

    third commit

--- testpull/src/testpull/testpull.java
+++ testpull/src/testpull/testpull.java
@@ -5,10 +5,14 @@ public class testpull {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test pull");
+
         //changes done by client2
 		
 	
 
+
+
+//stash changes temp
 
 	}
 
